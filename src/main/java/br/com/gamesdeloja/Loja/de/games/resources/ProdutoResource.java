4e655a79662e740d00e2b33a0212597538470fc1
italosantana <italosanttana@hotmail.com>
2021-06-18 11:56:09 -0300

    Criando o controller + endpoints

---
+++ src/main/java/br/com/gamesdeloja/Loja/de/games/resources/ProdutoResource.java
@@ -0,0 +1,46 @@
+package br.com.gamesdeloja.Loja.de.games.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// mapeando o end
+@RestController
+@RequestMapping(value="/api")
+public class ProdutoResource {
+	
+	@Autowired
+	ProdutoRepository produtoRepository;
+	
+	@GetMapping("/produto") // procurar por todos os produtos no endpoint
+	public List<Produto> ListaProdutos(){
+			return produtoRepository.findAll();
+	}
+	@GetMapping("/produto/{id}") // procurar por um produto em específico
+	public Produto listaProdutoUnico(@PathVariable(value="id") long id){
+			return produtoRepository.findBy(id);
+	}
+	
+	@PostMapping("/produto") // escolher o produto
+	public Produto salvaProduto(@RequestBody Produto produto) {
+			return produtoRepository.save(produto);
+	}
+	
+	@DeleteMapping("/produto") //excluir o produto
+	public void deletaProduto(@RequestBody Produto produto) {
+		produtoRepository.delete(produto);
+	}
+	
+	@PutMapping("/produto") // trocar o produto
+	public void atualizaProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
+}
